53300965937035386d4afd2704f0f95588067953
Sunil Sigar <sunil.s@westagilelabs.com>
2019-04-12 18:11:48 +0530

    Basic setup for login and register

--- app/components/Register.js
+++ app/components/Register.js
@@ -4,17 +4,20 @@ import { Link } from 'react-router-dom';
 import styles from './Register.css';
 import routes from '../constants/routes';
 
-
 type Props = {};
 
-export default class Register extends Component<Props>{
-    props: Props;
-    render() {
-        return (
-            <div className="main-div">
-            <p>This is a test div</p>
-            </div>
-        )
-    }
-    
+export default class Register extends Component<Props> {
+  props: Props;
+  render() {
+    return (
+      <div className="main-div">
+        <div className={styles.backButton} data-tid="backButton">
+          <Link to={routes.LOGIN}>
+            <i className="fa fa-arrow-left fa-3x" />
+          </Link>
+        </div>
+        <p>This is a test div</p>
+      </div>
+    );
+  }
 }
