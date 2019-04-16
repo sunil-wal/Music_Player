930831fd7243bab257aea1ed1d1501810e40164b
Sunil Sigar <sunil.s@westagilelabs.com>
2019-04-16 12:25:28 +0530

    updated home screen, added buttons for create windows

--- app/components/HomePage.js
+++ app/components/HomePage.js
@@ -1,56 +1,95 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col, ListGroup, ListGroupItem } from 'reactstrap';
+import {
+  TabContent,
+  TabPane,
+  Nav,
+  NavItem,
+  NavLink,
+  Card,
+  Button,
+  CardTitle,
+  CardText,
+  Row,
+  Col,
+  ListGroup,
+  ListGroupItem,
+  Pagination,
+  PaginationItem,
+  PaginationLink
+} from 'reactstrap';
 import classnames from 'classnames';
 import { userActions } from '../actions';
+import DataPagination from './DataPagination';
 
 function DataList(props) {
-    const names = props.names;
-    const listItems = names.map((name, index) =>
-      <ListGroupItem key={name + index}>{name}</ListGroupItem>
-
-    );
-    return (
-      <ListGroup>
-        {listItems}
-      </ListGroup>
-    );
-  }
+  const names = props.names;
+  const listItems = names.map((name, index) => (
+    <ListGroupItem key={name + index}>{name}</ListGroupItem>
+  ));
+  return (
+    <div>
+      <ListGroup>{listItems}</ListGroup>
+      <DataPagination itemsLength={listItems.length} />
+    </div>
+  );
+}
 
 class HomePage extends React.Component {
-    constructor(props) {
-      super(props);
+  constructor(props) {
+    super(props);
 
-      this.toggle = this.toggle.bind(this);
-      this.logoutFun = this.logoutFun.bind(this);
-      this.state = {
-        activeTab: '1'
-      };
-    }
-    toggle(tab) {
-      if (this.state.activeTab !== tab) {
-        this.setState({
-          activeTab: tab
-        });
-      }
-    }
-    logoutFun(){
-      this.props.dispatch(userActions.logout());
-    }
+    this.dataSet = [...Array(Math.ceil(Math.random() * 10))].map(
+      (a, i) => 'Record ' + (i + 1)
+    );
 
-    render() {
-        const { authentication } = this.props;
-        return (
-          <div>
-          <p>
-            <Button color="primary" onClick={this.logoutFun}>Logout</Button>
-                </p>
+    this.pageSize = 3;
+    this.pagesCount = Math.ceil(this.dataSet.length / this.pageSize);
+
+    this.handleToggle = this.handleToggle.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+    this.handlePageChange = this.handlePageChange.bind(this);
+    this.state = {
+      activeTab: '1',
+      currentPage: 0
+    };
+  }
+  handleToggle(tab) {
+    if (this.state.activeTab !== tab) {
+      this.setState({
+        activeTab: tab
+      });
+    }
+  }
+  handleLogout() {
+    this.props.dispatch(userActions.logout());
+  }
+  handlePageChange(e, index) {
+    e.preventDefault();
+
+    this.setState({
+      currentPage: index
+    });
+  }
+
+  render() {
+    const { authentication } = this.props;
+    return (
+      <div>
+      <div className="text-right">
+          <Button color="primary" onClick={this.handleLogout}>
+            Logout
+          </Button>
+        </div>
+        <div />
         <Nav tabs>
-        <NavItem>
+          <NavItem>
             <NavLink
               className={classnames({ active: this.state.activeTab === '1' })}
-              onClick={() => { this.toggle('1'); }}
+              onClick={() => {
+                this.handleToggle('1');
+              }}
             >
               Airtist
             </NavLink>
@@ -58,7 +97,9 @@ class HomePage extends React.Component {
           <NavItem>
             <NavLink
               className={classnames({ active: this.state.activeTab === '2' })}
-              onClick={() => { this.toggle('2'); }}
+              onClick={() => {
+                this.handleToggle('2');
+              }}
             >
               Albums
             </NavLink>
@@ -66,17 +107,21 @@ class HomePage extends React.Component {
           <NavItem>
             <NavLink
               className={classnames({ active: this.state.activeTab === '3' })}
-              onClick={() => { this.toggle('3'); }}
+              onClick={() => {
+                this.handleToggle('3');
+              }}
             >
-            Tracks
+              Tracks
             </NavLink>
           </NavItem>
           <NavItem>
             <NavLink
               className={classnames({ active: this.state.activeTab === '4' })}
-              onClick={() => { this.toggle('4'); }}
+              onClick={() => {
+                this.handleToggle('4');
+              }}
             >
-            Playlist
+              Playlist
             </NavLink>
           </NavItem>
         </Nav>
@@ -84,54 +129,53 @@ class HomePage extends React.Component {
           <TabPane tabId="1">
             <Row>
               <Col sm="12">
-               <DataList names={authentication.artistNames} />
+                <div className="text-right">
+                  <Button color="primary">Add Artist</Button>
+                </div>
+                <DataList names={authentication.artistNames} />
               </Col>
             </Row>
           </TabPane>
           <TabPane tabId="2">
             <Row>
               <Col sm="12">
-               <DataList names={authentication.albumNames} />
+                <div className="text-right">
+                  <Button color="primary">Add Album</Button>
+                </div>
+                <DataList names={authentication.albumNames} />
               </Col>
             </Row>
           </TabPane>
           <TabPane tabId="3">
             <Row>
               <Col sm="12">
-               <DataList names={authentication.tracks} />
+                <div className="text-right">
+                  <Button color="primary">Add Track</Button>
+                </div>
+                <DataList names={authentication.tracks} />
               </Col>
             </Row>
           </TabPane>
           <TabPane tabId="4">
             <Row>
               <Col sm="12">
-               <DataList names={authentication.plyalists} />
+                <div className="text-right">
+                  <Button color="primary">Add Playlist</Button>
+                </div>
+                <DataList names={authentication.plyalists} />
               </Col>
             </Row>
           </TabPane>
-
         </TabContent>
       </div>
-
-        );
-    }
+    );
+  }
 }
-// <div className="col-md-6 col-md-offset-3">
-//                 <h1>Hi</h1>
-//                 <h3>Artists:</h3>
-//                 <DataList names={authentication.artistNames} />
-//                 <h3>Albums:</h3>
-//                 <DataList names={authentication.albumNames} />
-//                 <h3>Tracks:</h3>
-//                 <DataList names={authentication.tracks} />
-//                 <h3>Playlist:</h3>
-//                 <DataList names={authentication.plyalists} />
-//             </div>
 function mapStateToProps(state) {
-    const { authentication } = state;
-    return {
-        authentication
-    };
+  const { authentication } = state;
+  return {
+    authentication
+  };
 }
 
 const connectedHomePage = connect(mapStateToProps)(HomePage);
--- app/reducers/authentication.reducer.js
+++ app/reducers/authentication.reducer.js
@@ -1,19 +1,27 @@
 import { userConstants } from '../constants';
 
-const artistNames = ['arijit', 'sonu', 'neha', 'bapi', 'justin', 'rihana', 'linkin'];
+const artistNames = [
+  'arijit',
+  'sonu',
+  'neha',
+  'bapi',
+  'justin',
+  'rihana',
+  'linkin'
+];
 const albumNames = ['kalank', 'kesari', 'gullyboy', 'simmba'];
 const tracks = ['Old Town Road', 'Sunflower', '7 Rings', 'Wow'];
 const plyalists = ['myfev', 'sunilfev', 'everyonesfev'];
 
 let user = JSON.parse(localStorage.getItem('user'));
-const initialState = user ? { loggedIn: true, user} : {};
+const initialState = user ? { loggedIn: true, user } : {};
 
 const musicData = {
   artistNames,
   albumNames,
   tracks,
   plyalists
-}
+};
 
 export function authentication(state = initialState, action) {
   switch (action.type) {
@@ -32,19 +40,19 @@ export function authentication(state = initialState, action) {
     case userConstants.LOGIN_FAILURE:
       return {
         ...musicData,
-        isLoggedIn: true,
+        isLoggedIn: false
       };
     case userConstants.LOGOUT:
       return {
         ...musicData,
-        isLoggedIn: false,
+        isLoggedIn: false
       };
     default:
       return {
         ...state,
         ...musicData,
-        isLoggedIn: false,
-      }
+        isLoggedIn: false
+      };
   }
 }
 
